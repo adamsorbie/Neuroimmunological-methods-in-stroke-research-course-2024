2454d8000b1b75820aa17940cff402f42ca83998
adamsorbie <adam.sorbie@med.uni-muenchen.de>
2023-06-19 15:43:41 +0200

    added installation script

--- install.R
+++ install.R
@@ -2,17 +2,25 @@
 if(!require("pacman")){
   install.packages("pacman", repos = "http://cran.us.r-project.org")
 }
-pacman::p_load(
-  BiocManager,
-  tidyverse,
-  EnhancedVolcano,
-  DESeq2,
-  clusterProfiler,
-  ggpubr,
-  ggsci,
-  rstatix,
-  phyloseq,
-  vegan,
-  ANCOMBC,
-  zoo
-)
+packages <- c("BiocManager",
+               "tidyverse",
+               "EnhancedVolcano",
+               "DESeq2",
+               "clusterProfiler",
+               "ggpubr",
+               "ggsci",
+               "rstatix",
+               "phyloseq",
+               "vegan",
+               "ANCOMBC",
+               "zoo")
+
+pacman::p_load(char = packages)
+
+uninstalled <- packages[!(packages %in% installed.packages()[,"Package"])]
+
+if (length(uninstalled) > 0){
+  print(paste0("installation not completed successfully, please install: ", uninstalled))
+} else {
+  print("Installed packages successfully :)")
+}
